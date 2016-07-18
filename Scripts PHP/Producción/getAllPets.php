3767b9b147fe3f43a5318303565f45786808d02f
rusifiqui <rusifiqui@gmail.com>
2016-07-18 14:27:02 +0200

    Añadida opción de mostrar todos los animales.

---
+++ Scripts PHP/Producción/getAllPets.php
@@ -0,0 +1,16 @@
+<?php
+mysql_connect("quiquevila.es.mysql","quiquevila_es","hNjPnF8Y");
+mysql_select_db("quiquevila_es");
+
+$post = json_decode(file_get_contents("php://input"), true);
+$state = $post['state'];
+
+$q=mysql_query("SELECT ID_PET, TYPE, RACE, LATITUDE, LONGITUDE, STATE, DESCRIPTION, ID_USER, FOUND_OR_LOST FROM PETS_DATA WHERE STATUS = 0");
+
+while($e=mysql_fetch_assoc($q))
+        $output[]=$e;
+
+print(json_encode($output));
+
+mysql_close();
+?>
